5a26c328a6e84f05f9debddf379047cc3e0eee87
DrewAwala24 <andrewawala141@gmail.com>
2026-06-24 22:07:19 +0300

    ML assignment using Linear Regression

---
+++ ice_cream_sales.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+data = {
+    "Temperature": [20, 25, 30, 35],
+    "Ice Cream Sales": [50, 80, 120, 180]
+}
+
+df = pd.DataFrame(data)
+
+X = df['Temperature'].values.reshape(-1,1)
+y = df['Ice Cream Sales'].values
+
+model = LinearRegression()
+model.fit(X,y)
+
+temp_to_predict = np.array([[40]])
+predicted_sales = model.predict(temp_to_predict)
+
+print("Estimated Ice Cream Sales at 40 degrees: {predicted_sales[0]:.2}")
+
+plt.scatter(X,y, color='purple', label='Actual Data')
+plt.plot(X, model.predict(X), color='red', label='Regression Line')
+plt.title('Temperature vs Ice Cream Sales')
+plt.xlabel('Temperature in Degrees Celcius')
+plt.ylabel('Ice Cream Sales')
+plt.legend()
+plt.show()
